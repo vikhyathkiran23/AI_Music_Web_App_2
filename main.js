e1dbee5bdc068b581eeb2b96a2dde137826d27fc
vikhyathkiran23 <72803191+vikhyathkiran23@users.noreply.github.com>
2022-08-22 20:50:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
-song_1="";
-song_2="";
+song_1 = "";
+song_2 = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+Song_status = ""
 
-function preload(){
+function preload() {
     song_1 = loadSound("music.mp3");
     song_2 = loadSound("music2.mp3");
-    
+
 }
-function setup(){
-    canvas=createCanvas(600,500);
+
+function setup() {
+    canvas = createCanvas(600, 500);
     canvas.center();
 
-    video=createCapture(VIDEO);
+    video = createCapture(VIDEO);
     video.hide();
 
     poseNet = ml5.poseNet(video, ModelLoaded);
@@ -24,6 +27,9 @@ function setup(){
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("Left Wrist X= " + leftWristX + " Left Wrist Y= " + leftWristY);
@@ -40,10 +46,22 @@ function ModelLoaded() {
     console.log("PoseNet is initialized");
 }
 
-function draw(){
+function draw() {
     image(video, 0, 0, 600, 500)
+
+    Song_status = song_1.isPlaying();
+
+    if (scoreLeftWrist > 0.2) {
+        fill("#FF0000")
+        stroke("#FF0000")
+        song_2.stop();
+        if(Song_status=="false"){
+            song_1.play();
+            document.getElementById("song_name").innerHTML="Song Name: " + "Peter Pan Song"
+        }
+    }
 }
 
-function play(){
+function play() {
     song.play();
 }
